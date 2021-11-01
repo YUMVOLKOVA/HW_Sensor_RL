2b13e83aed599184dc033c318f71bb2dbca495db
YUMVOLKOVA <60215607+YUMVOLKOVA@users.noreply.github.com>
2021-11-01 22:53:41 +0300

    try

---
+++ Modified_Dungeon.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from gym import spaces
+
+sys.path.append(os.path.abspath("mapgen"))
+os.environ["PYTHONPATH"] = os.path.abspath("mapgen")
+from mapgen import Dungeon
+
+class ModifiedDungeon(Dungeon):
+    def __init__(self, width=20, height=20, max_rooms=3, min_room_xy=5, max_room_xy=12,
+                 max_steps=1000, observation_size=11, vision_radius=5, seed = 10):
+        super().__init__(
+            width=width,
+            height=height,
+            max_rooms=max_rooms,
+            min_room_xy=min_room_xy,
+            max_room_xy=max_room_xy,
+            observation_size=observation_size,
+            vision_radius=vision_radius,
+            max_steps=max_steps
+        )
+
+        self.seed(seed)
+        self.observation_space = spaces.Box(0, 1, [observation_size, observation_size, 3])
+        self.action_space = spaces.Discrete(3)
+
+    def reset(self):
+        observation = super().reset()
+        return observation[:, :, :-1]
+
+    def step(self, action):
+        observation, reward, done, info = super().step(action)
+        observation = observation[:, :, :-1]
+        reward = reward - (info['step'] / self._max_steps) + (info['new_explored'] / info['step'])
+        return observation, reward, done, info
---
+++ train_val.py
@@ -0,0 +1,129 @@
+import os
+from os.path import join
+from pathlib import Path
+import ray
+import ray.rllib.agents.ppo as ppo
+from ray import tune
+import wandb
+from PIL import Image
+from Modified_Dungeon import ModifiedDungeon
+from tqmd import tqmd
+
+SAVE_PATH = os.getenv("HOME") + '/save/'
+CHECKPOINT_PATH = os.getenv("HOME") + '/ppo/'
+RUNS = 2
+TRAIN = True
+
+
+def logging_results(results, iteration):
+    wandb.log({'iteration': iteration + 1,
+              'episode_reward_min': results['episode_reward_min'],
+              'episode_reward_mean': results['episode_reward_mean'],
+              'episode_reward_max': results['episode_reward_max'],
+              'episode_len_mean': results['episode_len_mean']})
+
+seed = 10
+
+config = ppo.DEFAULT_CONFIG.copy()
+config['num_gpus'] = 1
+config['log_level'] = 'INFO'
+config['framework'] = 'torch'
+config['env'] = 'ModifiedDungeon'
+config['env_config'] = {
+    'width': 20,
+    'height': 20,
+    'max_rooms': 3,
+    'min_room_xy': 5,
+    'max_room_xy': 10,
+    'observation_size': 11,
+    'vision_radius': 5,
+    'max_steps': 1000
+}
+config['model'] = {
+    'conv_filters': [
+        [16, (3, 3), 2],
+        [32, (3, 3), 2],
+        [32, (3, 3), 1],
+    ],
+    'post_fcnet_hiddens': [32],
+    'post_fcnet_activation': 'relu',
+    'vf_share_layers': False,
+}
+config['rollout_fragment_length'] = 100
+config['entropy_coeff'] = 0.1
+config['lambda'] = 0.95
+config['vf_loss_coeff'] = 1.0
+
+def train(agent, save_path, iterations = 200):
+    checkpoint_dir = join(save_path, "checkpoints")
+    gif_dir = join(save_path, "gifs")
+
+    for n in tqmd(range(iterations)):
+        result = agent.train()
+        file_name = agent.save(checkpoint_dir)
+        logging_results(result, n)
+
+        if (n + 1) % 5 == 0:
+            artifact = wandb.Artifact("model", type="model")
+            artifact.add_dir(Path(file_name).parent.absolute())
+            wandb.log_artifact(artifact)
+
+            env = ModifiedDungeon(20, 20, 3, min_room_xy=5, max_room_xy=10, vision_radius=5)
+            env.seed(seed)
+            env.action_space.seed(seed)
+
+            obs = env.reset()
+
+            frames = []
+
+            for _ in range(500):
+                action = agent.compute_single_action(obs)
+
+                frame = Image.fromarray(env._map.render(env._agent)).convert('RGB').resize((500, 500), Image.NEAREST).quantize()
+                frames.append(frame)
+
+                obs, reward, done, info = env.step(action)
+                if done:
+                    break
+            path_for_output = join(gif_dir, f'output_{n + 1}.gif')
+            wandb.log({'gifs': wandb.Video(path_for_output, fps=30, format='gif')})
+            frames[0].save(path_for_output, save_all=True, append_images=frames[1:], loop=0, duration=1000/60)
+
+def val(agent, save_path, iters):
+    env = ModifiedDungeon(20, 20, 3, min_room_xy=5, max_room_xy=10, vision_radius=5)
+    obs = env.reset()
+
+    for i in iters:
+        frames = []
+
+        for _ in range(500):
+            action = agent.compute_single_action(obs)
+
+            frame = Image.fromarray(env._map.render(env._agent)).convert('RGB').resize((500, 500), Image.NEAREST).quantize()
+            frames.append(frame)
+
+            obs, reward, done, info = env.step(action)
+            if done:
+                break
+        path_for_output = join(save_path, f'val_{n + 1}.gif')
+        frames[0].save(path_for_output, save_all=True, append_images=frames[1:], loop=0, duration=1000/60)
+        obs = env.reset()
+
+
+
+if TRAIN == True:
+    ray.shutdown()
+    ray.init(ignore_reinit_error=True)
+    wandb.init(project='ProdStory-Sensor', entity="yumvolkova", config=config)
+    tune.register_env("ModifiedDungeon", lambda config: ModifiedDungeon(**config))
+
+    agent = ppo.PPOTrainer(config)
+    train(agent, SAVE_PATH)
+else:
+    ray.shutdown()
+    ray.init(ignore_reinit_error=True)
+    tune.register_env("ModifiedDungeon", lambda config: ModifiedDungeon(**config))
+    agent = ppo.PPOTrainer(config)
+    agent.restore(CHECKPOINT_PATH)
+    val(agent, SAVE_PATH, RUNS)
+
